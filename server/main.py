df7d2cbecb77cb1978e659a94f8326efe54fd663
jochana <c01034784796@gmail.com>
2021-02-18 14:55:47 +0900

    Change Server Framework - FastApi to Flask

--- server/main.py
+++ server/main.py
@@ -1,9 +1,13 @@
-from fastapi import FastAPI
+from flask import Flask, make_response
+import os
+
+app = Flask(__name__)
 
 
-app = FastAPI()
+app.route('/')
+def health_check():
+	return make_response(200)
 
 
-@app.get('/')
-def index():
-	return {"connection": "success"}
+if __name__ == '__main__':
+	app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
