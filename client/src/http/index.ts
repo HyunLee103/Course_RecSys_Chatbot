ca1fac760d74d8fa42a369e38a87c15675bee222
Byeong Gyu Choi <bisschoi9541@gmail.com>
2021-02-20 15:59:19 +0900

    Http Client 수정

--- client/src/http/index.ts
+++ client/src/http/index.ts
@@ -1,6 +1,34 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 
-export const restAPI = axios.create({ baseURL: '' });
+interface ChatbotData {
+  ins_id: string;
+  intent_id: string;
+  param_id: string;
+  user_id: string;
+  parameters: {
+    강의: string;
+    선호분야: string;
+    오전수업: string;
+    이러닝: string;
+    팀플: string;
+    학점: string;
+  };
+  result: {
+    message: string;
+    carouselList: any[];
+  };
+}
+
+interface ChatbotResponseFormat {
+  code: string;
+  result: ChatbotData;
+}
+
+export type ChatbotResponse = AxiosResponse<ChatbotResponseFormat>;
+
+export const restAPI = axios.create({
+  baseURL: 'https://chatbot-tsy3pfrytq-du.a.run.app/',
+});
 
 export const chatbot = axios.create({
   baseURL:
